7e97854e2a75f639f8bf0b592a02d460a1ec8b32
shah 911 <mianshahzeb911@gmail.com>
2024-12-28 00:45:36 +0500

    add animation while removing cartItems

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@
 import { CloseOutlined } from "@mui/icons-material";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import gql from "graphql-tag";
 import { print } from "graphql";
 import { formatCurrency, storeFront } from "../../../utils";
@@ -292,11 +292,34 @@ function Cart() {
     return <ErrPage />;
   }
 
-  return isLoading ? (
-    <div className="h-[100vh] flex items-center justify-center">
-      <Loader />
-    </div>
-  ) : (
+  if (isLoading) {
+    return (
+      <div className="h-[100vh] flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (!data?.cart.lines?.edges.length) {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="h-[100vh] flex flex-col items-center justify-center gap-3"
+      >
+        <span className="text-base font-[300]">
+          Your shopping bag is currently empty.
+        </span>
+        <Link href="/">
+          <button className="btn-secondary uppercase border border-gray-400 text-lg px-6 py-2">
+            Keep shopping
+          </button>
+        </Link>
+      </motion.div>
+    );
+  }
+
+  return (
     <div className="flex items-center justify-center">
       <div
         className={`h-[100vh] w-[100%] flex items-center justify-center fixed top-0 bg-[rgba(255,255,255,.5)] 
@@ -323,27 +346,31 @@ function Cart() {
           </motion.div>
         )}
       </AnimatePresence>
-      {data?.cart.lines?.edges.length > 0 ? (
-        <div className="lg:h-[auto] flex items-center justify-center py-14">
-          <div className="h-[85%] w-[95vw] mx-auto flex flex-col lg:flex-row">
-            <div className="flex-[3]">
-              <div className="flex items-center justify-between">
-                <h1 className="uppercase font-[300] text-4xl text-[#c40d2e] tracking-[3px]">
-                  my cart
-                </h1>
-                <button
-                  onClick={() => {
-                    setRemovingItem(true);
-                    emptyCartMutation.mutate();
-                  }}
-                  className="btn-secondary cursor-pointer p-3 border border-black"
-                >
-                  empty cart
-                </button>
-              </div>
-              {data?.cart.lines.edges?.map((item: CartItem, i: number) => (
-                <div
-                  key={i}
+
+      <div className="lg:h-[auto] flex items-center justify-center py-14">
+        <div className="h-[85%] w-[95vw] mx-auto flex flex-col lg:flex-row">
+          <div className="flex-[3]">
+            <div className="flex items-center justify-between">
+              <h1 className="uppercase font-[300] text-4xl text-[#c40d2e] tracking-[3px]">
+                my cart
+              </h1>
+              <button
+                onClick={() => {
+                  setRemovingItem(true);
+                  emptyCartMutation.mutate();
+                }}
+                className="btn-secondary cursor-pointer p-3 border border-black"
+              >
+                empty cart
+              </button>
+            </div>
+            {/* singleCartItem */}
+            <AnimatePresence mode="sync">
+              {data?.cart.lines.edges?.map((item: CartItem) => (
+                <motion.div
+                  key={item.node.id}
+                  exit={{ clipPath: "inset(0 0 100% 0)" }}
+                  layout
                   className="flex items-center justify-between h-[220px] lg:w-[75%] 2xl:w-[80%] 2xl:h-[300px] w-[100%] my-10"
                 >
                   {/* ProductImg */}
@@ -359,7 +386,7 @@ function Cart() {
                   </div>
                   {/* Details */}
                   <div className="flex-[2] h-[100%] flex flex-col pl-5 md:pl-10 gap-4">
-                    <span className="text-xs md:text-base font-[300]">
+                    <span className="text-xs md:text-base font-[300] capitalize">
                       {item.node.merchandise.product.tags[0]}
                     </span>
                     <span className="text-xs md:text-sm font-[500]">
@@ -400,52 +427,41 @@ function Cart() {
                       {item.node.quantity}
                     </span>
                   </div>
-                </div>
+                </motion.div>
               ))}
-            </div>
-            <div className="flex-[1] flex flex-col gap-5 items-center lg:justify-start justify-center py-[40.5px] border border-black rounded-lg lg:border-none">
-              <div className="flex flex-col gap-5">
-                <h1 className="uppercase font-[300] text-3xl text-[#c40d2e] tracking-[3px]">
-                  order now
-                </h1>
-                <span className="text-base font-[300]">
-                  Sub-Total :{" "}
-                  {formatCurrency(
-                    data?.cart?.cost?.subtotalAmount?.amount,
-                    data?.cart?.cost?.subtotalAmount?.currencyCode
-                  )}
-                </span>
-                <span className="text-base font-[300]">
-                  Total :{" "}
-                  {formatCurrency(
-                    data?.cart?.cost?.totalAmount?.amount,
-                    data?.cart?.cost?.totalAmount?.currencyCode
-                  )}
-                </span>
-                {checkoutUrl && (
-                  <button
-                    onClick={() => (window.location.href = checkoutUrl)}
-                    className="btn-secondary z-10 uppercase border border-gray-400 text-lg px-6 py-2 text-center"
-                  >
-                    checkout
-                  </button>
+            </AnimatePresence>
+          </div>
+          <div className="flex-[1] flex flex-col gap-5 items-center lg:justify-start justify-center py-[40.5px] border border-black rounded-lg lg:border-none">
+            <div className="flex flex-col gap-5">
+              <h1 className="uppercase font-[300] text-3xl text-[#c40d2e] tracking-[3px]">
+                order now
+              </h1>
+              <span className="text-base font-[300]">
+                Sub-Total :{" "}
+                {formatCurrency(
+                  data?.cart?.cost?.subtotalAmount?.amount,
+                  data?.cart?.cost?.subtotalAmount?.currencyCode
                 )}
-              </div>
+              </span>
+              <span className="text-base font-[300]">
+                Total :{" "}
+                {formatCurrency(
+                  data?.cart?.cost?.totalAmount?.amount,
+                  data?.cart?.cost?.totalAmount?.currencyCode
+                )}
+              </span>
+              {checkoutUrl && (
+                <button
+                  onClick={() => (window.location.href = checkoutUrl)}
+                  className="btn-secondary z-10 uppercase border border-gray-400 text-lg px-6 py-2 text-center"
+                >
+                  checkout
+                </button>
+              )}
             </div>
           </div>
         </div>
-      ) : (
-        <div className="h-[100vh] flex flex-col items-center justify-center gap-3">
-          <span className="text-base font-[300]">
-            Your shopping bag is currently empty.
-          </span>
-          <Link href="/">
-            <button className="btn-secondary uppercase border border-gray-400 text-lg px-6 py-2">
-              Keep shopping
-            </button>
-          </Link>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
